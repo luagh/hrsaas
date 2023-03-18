b930a6af085179421075d79b9e2c2662385c6c1f
laugh <laugh@email.com>
2023-03-18 15:02:24 +0800

    登录界面表单的基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title"><img src="@/assets/common/login-logo.png" alt=""></h3>
       </div>
 
       <el-form-item prop="username">
@@ -41,12 +41,12 @@
         </span>
       </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
+      <el-button class="loginBtn" :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+      <!-- 提示文本和登录文本 -->
+         <div class="tips">
+        <span style="margin-right:20px;">账号: 13800000002</span>
+        <span> 密码: 123456</span>
+   </div>
 
     </el-form>
   </div>
@@ -130,7 +130,7 @@ export default {
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
 $bg:#283443;
-$light_gray:#fff;
+$light_gray:#68b0fe; // 将输入框颜色改成蓝色
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -141,6 +141,9 @@ $cursor: #fff;
 
 /* reset element-ui css */
 .login-container {
+  //在样式表中使用`@`别名的时候，需要在@前面加上一个`~`*符号，否则不识别
+  background-image: url('~@/assets/common/login.jpg'); // 设置背景图片
+  background-position: center; // 将图片位置设置为充满整个屏幕
   .el-input {
     display: inline-block;
     height: 47px;
@@ -149,7 +152,8 @@ $cursor: #fff;
     input {
       background: transparent;
       border: 0px;
-      -webkit-appearance: none;
+       -webkit-appearance: none;
+  appearance: none;
       border-radius: 0px;
       padding: 12px 5px 12px 15px;
       color: $light_gray;
@@ -165,10 +169,20 @@ $cursor: #fff;
 
   .el-form-item {
     border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    background: rgba(255, 255, 255, 0.7);// 输入登录表单的背景色
     border-radius: 5px;
     color: #454545;
   }
+  
+   .el-form-item__error {
+    color: #fff
+  }
+  .loginBtn {
+  background: #407ffe;
+  height: 64px;
+  line-height: 32px;
+  font-size: 24px;
+}
 }
 </style>
 
