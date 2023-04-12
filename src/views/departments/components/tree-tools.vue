37771991a7462013ad44d39e11d4c7bb8028266d
laugh <laugh@email.com>
2023-04-12 16:52:17 +0800

    组织架构模块 下拉菜单的注册事件

--- src/views/departments/components/tree-tools.vue
+++ src/views/departments/components/tree-tools.vue
@@ -10,15 +10,16 @@
         <el-col>{{ treeNode.manager }}</el-col>
         <el-col>
           <!-- 下拉菜单 element -->
-          <el-dropdown>
+          <el-dropdown @command="operateDepts">
             <span>
               操作<i class="el-icon-arrow-down" />
             </span>
             <!-- 下拉菜单 -->
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>添加子部门</el-dropdown-item>
-              <el-dropdown-item v-if="!isRoot">编辑部门</el-dropdown-item>
-              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+              <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+              <!-- 编辑部门和删除部门只会在子节点上显示 -->
+              <el-dropdown-item v-if="!isRoot" command="edit">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot" command="del">删除部门</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
         </el-col>
@@ -38,6 +39,17 @@ export default {
     isRoot: {
       type: Boolean,
       default: false
+    }
+  },
+  methods: {
+    operateDepts(type) {
+      if (type === 'add') {
+
+      } else if (type === 'edit') {
+
+      } else {
+        console.log('ss');
+      }
     }
   }
 }
