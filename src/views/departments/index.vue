0be56123d5aa826f571dc5b9a5b1dc467e08480b
laugh <laugh@email.com>
2023-04-12 16:33:06 +0800

    动态获取组织架构数据

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,75 +1,50 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
+      <!-- 实现页面的基本布局 -->
       <el-card class="tree-card">
-        <el-row type="flex" justify="space-between" align="middle" style="height: 40px">
-          <el-col>
-            <span>测试科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>负责人</el-col>
-              <el-col>
-                <!-- 下拉菜单 element -->
-                <el-dropdown>
-                  <span> 操作<i class="el-icon-arrow-down" /></span>
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-
-          </el-col>
-        </el-row>
-        <el-tree :data="departs" :props="defaultProps" :default-expand-all="true">
-          <!-- 传入内容 插槽内容 会循环多次 有多少节点 就循环多少次 -->
-          <!-- 作用域插槽 slot-scope="obj" 接收传递给插槽的数据   data 每个节点的数据对象-->
-          <el-row slot-scope="{ data }" type="flex" justify="space-between" align="middle"
-            style="height: 40px; width: 100%">
-            <el-col>
-              <!-- 左侧内容 -->
-              <span>{{ data.name }}</span>
-            </el-col>
-            <el-col :span="4">
-              <el-row type="flex" justify="end">
-                <el-col>{{ data.manager }}</el-col>
-                <el-col>
-                  <!-- 放置下拉菜单 -->
-                  <el-dropdown>
-                    <!-- 内容 -->
-                    <span>操作
-                      <i class="el-icon-arrow-down" />
-                    </span>
-                    <!-- 具名插槽 -->
-                    <el-dropdown-menu slot="dropdown">
-                      <!-- 下拉选项 -->
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-
-                    </el-dropdown-menu>
-                  </el-dropdown>
-                </el-col>
-              </el-row>
-
-              <!-- 右侧内容 -->
-            </el-col>
-          </el-row> </el-tree>
+        <!-- 用了一个行列布局 -->
+        <!-- 缺少treeNode -->
+        <tree-tools :tree-node="company" :is-root="true" />
+        <!--放置一个属性   这里的props和我们之前学习的父传子 的props没关系-->
+        <el-tree :data="departs" :props="defaultProps" default-expand-all>
+          <!-- 说明el-tree里面的这个内容 就是插槽内容 => 填坑内容  => 有多少个节点循环多少次 -->
+          <!-- scope-scope 是 tree组件传给每个节点的插槽的内容的数据 -->
+          <!-- 顺序一定是 执行slot-scope的赋值 才去执行 props的传值 -->
+          <tree-tools slot-scope="{ data }" :tree-node="data" />
+        </el-tree>
       </el-card>
     </div>
   </div>
 </template>
 
+
 <script>
+import TreeTools from './components/tree-tools'
+import { getDepartments } from '@/api/departments'
+import { tranListToTreeData } from '@/utils/index'
 export default {
+  components: {
+    TreeTools
+  },
   data() {
     return {
+      company: {}, // 就是头部的数据结构
+      departs: [],
       defaultProps: {
-        label: 'name'
-      },
-      departs: [{ name: '总裁办', children: [{ name: '董事会' }] },
-      { name: '行政部' }, { name: '人事部' }]
+        label: 'name' // 表示 从这个属性显示内容
+      }
+    }
+  },
+  created() {
+    this.getDepartments() // 调用自身的方法
+  },
+  methods: {
+    async getDepartments() {
+      const result = await getDepartments()
+      this.company = { name: result.companyName, manager: '负责人' }
+      // 这里定义一个空串  因为 它是根 所有的子节点的数据pid 都是 ""
+      this.departs = tranListToTreeData(result.depts, '')
     }
   }
 }
