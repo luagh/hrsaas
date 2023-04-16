f609fa599f71b1bfddc07fe7d2e3b38e09262bd9
laugh <laugh@email.com>
2023-04-16 14:22:59 +0800

    员工设置模块新增员工弹框下拉数据和确定取消按钮功能的实现

--- src/views/employees/components/add-employee.vue
+++ src/views/employees/components/add-employee.vue
@@ -1,7 +1,12 @@
 <template>
   <el-dialog title="新增员工" :visible="showDialog">
     <!-- 表单 -->
-    <el-form :model="formData" :rules="rules" label-width="120px">
+    <el-form
+      ref="addEmployee"
+      :model="formData"
+      :rules="rules"
+      label-width="120px"
+    >
       <el-form-item label="姓名" prop="username">
         <el-input
           v-model="formData.username"
@@ -28,7 +33,15 @@
           v-model="formData.formOfEmployment"
           style="width: 50%"
           placeholder="请选择"
-        />
+        >
+          <!-- 遍历只能遍历组件的数据 -->
+          <el-option
+            v-for="item in EmployeeEnum.hireType"
+            :key="item.id"
+            :label="item.value"
+            :value="item.id"
+          />
+        </el-select>
       </el-form-item>
       <el-form-item label="工号" prop="workNumber">
         <el-input
@@ -42,6 +55,16 @@
           v-model="formData.departmentName"
           style="width: 50%"
           placeholder="请选择部门"
+          @focus="getDepartments"
+        />
+        <!-- 放置一个tree组件 -->
+        <el-tree
+          v-if="showTree"
+          v-loading="loading"
+          :data="treeData"
+          default-expand-all=""
+          :props="{ label: 'name' }"
+          @node-click="selectNode"
         />
       </el-form-item>
       <el-form-item label="转正时间" prop="correctionTime">
@@ -56,8 +79,8 @@
     <template v-slot:footer>
       <el-row type="flex" justify="center">
         <el-col :span="6">
-          <el-button size="small">取消</el-button>
-          <el-button type="primary" size="small">确定</el-button>
+          <el-button size="small" @click="btnCancel">取消</el-button>
+          <el-button type="primary" size="small" @click="btnOK">确定</el-button>
         </el-col>
       </el-row>
     </template>
@@ -66,7 +89,9 @@
 
 <script>
 import EmployeeEnum from "@/api/constant/employees";
-
+import { getDepartments } from "@/api/departments";
+import { transListToTreeData } from "@/utils/index";
+import { addEmployee } from "@/api/employees";
 export default {
   props: {
     showDialog: {
@@ -78,9 +103,7 @@ export default {
     return {
       EmployeeEnum, // 在data中定义数据
       // 表单数据
-      treeData: [], // 定义数组接收树形数据
-      showTree: false, // 控制树形的显示或者隐藏
-      loading: false, // 控制树的显示或者隐藏进度条
+
       formData: {
         username: "",
         mobile: "",
@@ -118,7 +141,53 @@ export default {
         ],
         timeOfEntry: [{ required: true, message: "入职时间", trigger: "blur" }],
       },
+      treeData: [], // 定义数组接收树形数据
+      showTree: false, // 控制树形的显示或者隐藏
+      loading: false, // 控制树的显示或者隐藏进度条
     };
+  },
+  methods: {
+    async getDepartments() {
+      this.showTree = true;
+      this.loading = true;
+      const { depts } = await getDepartments();
+      // depts是数组 但不是树形
+      this.treeData = transListToTreeData(depts, "");
+      this.loading = false;
+    },
+    selectNode(node) {
+      this.formData.departmentName = node.name;
+      this.showTree = false;
+    },
+    // 点击确定时 校验整个表单
+    async btnOK() {
+      try {
+        await this.$refs.addEmployee.validate();
+        // 调用新增接口
+        await addEmployee(this.formData); // 新增员工
+        // 告诉父组件更新数据
+        // this.$parent 可以直接调用到父组件的实例 实际上就是父组件this
+        // this.$emit
+        this.$parent.getEmployeeList();
+        this.$parent.showDialog = false;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    btnCancel() {
+      // 重置原来的数据
+      this.formData = {
+        username: "",
+        mobile: "",
+        formOfEmployment: "",
+        workNumber: "",
+        departmentName: "",
+        timeOfEntry: "",
+        correctionTime: "",
+      };
+      this.$refs.addEmployee.resetFields(); // 重置校验结果
+      this.$emit("update:showDialog", false);
+    },
   },
 };
 </script>
