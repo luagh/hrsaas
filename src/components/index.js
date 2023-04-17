09c67935fc7166bf183baa3bb715a2b904867a03
laugh <laugh@email.com>
2023-04-17 11:40:50 +0800

    员工设置模块-导入功能的实现

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import PageTools from './PageTools'
+import UploadExcel from './UploadExcel'
 export default {
     install(Vue) {
-        //  注册全局的通用栏组件对象
-        Vue.component('PageTools', PageTools)
+        Vue.component('PageTools', PageTools) // 注册工具栏组件
+        Vue.component('UploadExcel', UploadExcel) // 注册导入excel组件
     }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -39,35 +39,43 @@ import Layout from '@/layout'
  * all roles can be accessed
  */
 export const constantRoutes = [{
-        path: '/login',
+    path: '/login',
+    component: () =>
+        import('@/views/login/index'),
+    hidden: true
+},
+
+{
+    path: '/404',
+    component: () =>
+        import('@/views/404'),
+    hidden: true
+},
+
+{
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+        path: 'dashboard',
+        name: 'Dashboard',
         component: () =>
-            import ('@/views/login/index'),
-        hidden: true
-    },
+            import('@/views/dashboard/index'),
+        meta: { title: 'Dashboard', icon: 'dashboard' }
+    }]
+},
+{
+    path: '/import',
+    component: Layout,
+    hidden: true, // 隐藏在左侧菜单中
+    children: [{
+        path: '', // 二级路由path什么都不写 表示二级默认路由
+        component: () => import('@/views/import')
+    }]
+},
 
-    {
-        path: '/404',
-        component: () =>
-            import ('@/views/404'),
-        hidden: true
-    },
-
-    {
-        path: '/',
-        component: Layout,
-        redirect: '/dashboard',
-        children: [{
-            path: 'dashboard',
-            name: 'Dashboard',
-            component: () =>
-                import ('@/views/dashboard/index'),
-            meta: { title: 'Dashboard', icon: 'dashboard' }
-        }]
-    },
-
-
-    // 404 page must be placed at the end !!!
-    { path: '*', redirect: '/404', hidden: true }
+// 404 page must be placed at the end !!!
+{ path: '*', redirect: '/404', hidden: true }
 ]
 
 export const asyncRoutes = [
