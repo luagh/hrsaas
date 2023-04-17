7e00d5810c26dee7fb7c9b1e91bb22418053cdf6
laugh <laugh@email.com>
2023-04-17 15:26:02 +0800

    员工设置模块-导出功能的实现

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -4,8 +4,15 @@
       <page-tools :show-before="true">
         <span slot="before">共{{ page.total }}条记录</span>
         <template slot="after">
-          <el-button size="small" type="warning" @click="$router.push('/import?type=user')">导入</el-button>
-          <el-button size="small" type="danger">导出</el-button>
+          <el-button
+            size="small"
+            type="warning"
+            @click="$router.push('/import?type=user')"
+            >导入</el-button
+          >
+          <el-button size="small" type="danger" @click="exportData"
+            >导出</el-button
+          >
           <el-button
             icon="plus"
             type="primary"
@@ -86,6 +93,7 @@
 import { getEmployeeList, delEmployee } from "@/api/employees";
 import EmployeeEnum from "@/api/constant/employees";
 import AddEmployee from "./components/add-employee.vue";
+import { formatDate } from "@/filters";
 
 export default {
   components: { AddEmployee },
@@ -125,11 +133,71 @@ export default {
       try {
         await this.$confirm("您确定删除该员工吗");
         await delEmployee(id);
-        this.getEmployeeList();
         this.$message.success("删除员工成功");
+        this.getEmployeeList();
       } catch (error) {
         console.log(error);
       }
+    },
+    exportData() {
+      const headers = {
+        姓名: "username",
+        手机号: "mobile",
+        入职日期: "timeOfEntry",
+        聘用形式: "formOfEmployment",
+        转正日期: "correctionTime",
+        工号: "workNumber",
+        部门: "departmentName",
+      };
+      // 导出excel
+      import("@/vendor/Export2Excel").then(async (excel) => {
+        //  excel是引入文件的导出对象
+        // 导出  header从哪里来
+        // data从哪里来
+        // 现在没有一个接口获取所有的数据
+        // 获取员工的接口 页码 每页条数    100   1 10000
+        const { rows } = await getEmployeeList({
+          page: 1,
+          size: this.page.total,
+        });
+        const data = this.formatJson(headers, rows); // 返回的data就是 要导出的结构
+        const multiHeader = [["姓名", "主要信息", "", "", "", "", "部门"]];
+        const merges = ["A1:A2", "B1:F1", "G1:G2"];
+        excel.export_json_to_excel({
+          header: Object.keys(headers),
+          data,
+          filename: "员工资料表",
+          multiHeader, // 复杂表头
+          merges, // 合并选项
+        });
+      });
+    },
+    // 将表头数据和数据进行对应
+    // [{}]  =>   [[]]
+    formatJson(headers, rows) {
+      return rows.map((item) => {
+        // item是一个对象  { mobile: 132111,username: '张三'  }
+        // ["手机号", "姓名", "入职日期" 。。]
+        return Object.keys(headers).map((key) => {
+          // 需要判断 字段
+          if (
+            headers[key] === "timeOfEntry" ||
+            headers[key] === "correctionTime"
+          ) {
+            // 格式化日期
+            return formatDate(item[headers[key]]);
+          } else if (headers[key] === "formOfEmployment") {
+            const obj = EmployeeEnum.hireType.find(
+              (obj) => obj.id === item[headers[key]]
+            );
+            return obj ? obj.value : "未知";
+          }
+          return item[headers[key]];
+        });
+        // ["132", '张三’， ‘’，‘’，‘’d]
+      });
+      // return rows.map(item => Object.keys(headers).map(key => item[headers[key]]))
+      // 需要处理时间格式问题
     },
   },
 };
