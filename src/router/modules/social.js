f7cb50b1f2587db47f9e5b6d8cee55dded3da41c
laugh <laugh@email.com>
2023-04-19 20:20:15 +0800

    社保页面数据的展示

--- src/router/modules/social.js
+++ src/router/modules/social.js
@@ -1,18 +1,50 @@
-import Layout from '@/layout'
-// 路由规则
-export default {
-    path: '/social', //路由地址
-    name: 'social',
-    component: Layout,
-    // 配置二级路的路由表
-    children: [{
 
-        path: '',
-        component: () =>
-            import ('@/views/social'),
-        meta: {
-            title: '社保',
-            icon: 'table'
-        }
-    }]
+import Layout from '@/layout'
+
+export default {
+  path: '/social_securitys',
+  component: Layout,
+  name: 'social_securitys',
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/social'),
+      name: 'social_securitys',
+      meta: {
+        title: '社保',
+        icon: 'table'
+
+      }
+    },
+    // 报表
+    {
+      path: 'detail/:id',
+      hidden: true,
+      component: () => import('@/views/social/detail'),
+      name: 'socialDetail',
+      meta: {
+        title: '社保'
+      }
+    },
+    // 历史归档
+    {
+      path: 'historicalArchiving',
+      hidden: true,
+      component: () => import('@/views/social/historical'),
+      name: 'socialHistorical',
+      meta: {
+        title: '历史归档'
+      }
+    },
+    // 月报表
+    {
+      path: 'monthStatement',
+      component: () => import('@/views/social/month'),
+      name: 'socialMonthStatement',
+      hidden: true,
+      meta: {
+        title: '当月报表'
+      }
+    }
+  ]
 }
